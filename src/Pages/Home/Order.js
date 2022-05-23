d6a5185893c5ef0ce60f1e98fc48ca1aad0b6b89
Md Mamun <juborajvai22@gmail.com>
2022-05-23 14:55:48 +0600

    update

--- src/Pages/Home/Order.js
+++ src/Pages/Home/Order.js
@@ -4,30 +4,64 @@ import { useNavigate, useParams } from 'react-router-dom';
 import Loading from '../Sheard/Loading';
 
 const Order = () => {
-      const {id} = useParams()
-      const {data , isLoading} = useQuery(['/prodcut' , id] ,() => fetch(`http://localhost:5000/product/${id}`).then(res => res.json()) )
+      const { id } = useParams()
+      const [order , setOrder] = useState(1)
+      const { data, isLoading } = useQuery(['/prodcut', id], () => fetch(`http://localhost:5000/product/${id}`).then(res => res.json()))
+
+
+
+      if (isLoading) {
+            return <Loading></Loading>
+      }
+      
+      // quentity decrease 
+      const decreacseQouentity =() =>{
+            let quentityOrder = order + 1
+            setOrder(quentityOrder)
+            
+      }
+      // quentity incrase 
+      const incarseQuentity = () =>{
+
+            if(order > 1){
+             let quentityOrder = order - 1
+             setOrder(quentityOrder)
+
+            }
+           
+      }
 
       
 
-      if(isLoading){
-            return <Loading></Loading>
-      }
-      console.log(data);
     
+
 
       return (
             <div className='max-w-7xl m-auto mt-20 px-4'>
                   <div class="card lg:card-side  shadow-xl">
                         <figure>
-                              <img className='myOrderImg' src={data?.img} alt="product"/></figure>
+                              <img className='myOrderImg' src={data?.img} alt="product" /></figure>
                         <div class="card-body ">
-                              <h2 class="card-title">Prodcut Name: {data?.name}</h2>
-                              <span className='my-0'>Decription: {data?.comment}</span>
-                              <span className='my-0'>Mimimun Quentity: {data?.minQuentity}</span>
-                              <span className='my-0'>Available Quentity: {data?.abalivaleQuentity}</span>
-                              <span>Price: {data?.price}</span>
+                              <h2 class="card-title text-3xl">Prodcut Name: {data?.name}</h2>
+                              <span className='text-xl'>Decription: {data?.comment}</span>
+                              <span className='text-xl'>Mimimun Orded Quentity: {data?.minQuentity} Psc</span>
+                              <span className='text-xl'>Available Quentity: {data?.abalivaleQuentity} Psc</span>
+                              <span className='text-xl font-bold'>Price: ${data?.price}</span>
+
+                              {/* menimam quentity feild  */}
+
+                              <div class="form-control mt-5">
+                                    <label class="label">
+                                          <span class="label-text">Enter Mimimun Quentity</span>
+                                    </label>
+                                    <label class="input-group">
+                                         <button onClick={incarseQuentity} className='btn'>-</button>
+                                          <input type="text" value={order} class="input input-bordered" />
+                                          <button onClick={decreacseQouentity} className='btn'>+</button>
+                                    </label>
+                              </div>
                               <div class="card-actions justify-end">
-                                    <button class="btn btn-primary">Listen</button>
+                                    <button  disabled={order < 20} class="btn btn-primary">Confrom Order</button>
                               </div>
                         </div>
                   </div>
